2555ee7c6a6febe46697fc85606cca47c943b69f
Vladimir Vasyukov <vladimir.vasyukov7@gmail.com>
2021-11-29 21:43:33 +0300

    Correction of remarks

--- src/main/java/com/epam/rd/autocode/EmployeeSetMapper.java
+++ src/main/java/com/epam/rd/autocode/EmployeeSetMapper.java
@@ -15,7 +15,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class EmployeeSetMapper implements SetMapper<Set<Employee>> {
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger log = LogManager.getLogger();
     private static final byte COLUMN_ID = 1;
     private static final byte COLUMN_FIRST_NAME = 2;
     private static final byte COLUMN_LAST_NAME = 3;
@@ -24,55 +24,50 @@ public class EmployeeSetMapper implements SetMapper<Set<Employee>> {
     private static final byte COLUMN_MANAGER = 6;
     private static final byte COLUMN_HIREDATE = 7;
     private static final byte COLUMN_SALARY = 8;
-    private BigInteger id;
-    private FullName fullName;
-    private Position position;
-    private LocalDate hired;
-    private BigDecimal salary;
 
     @Override
     public Set<Employee> mapSet(ResultSet resultSet) {
         Set<Employee> employeeSet = new HashSet<>();
         try {
             while (resultSet.next()) {
-                id = new BigInteger(String.valueOf(resultSet.getLong(COLUMN_ID)));
-                fullName = new FullName(
+                BigInteger id = new BigInteger(String.valueOf(resultSet.getLong(COLUMN_ID)));
+                FullName fullName = new FullName(
                     resultSet.getString(COLUMN_FIRST_NAME),
                     resultSet.getString(COLUMN_LAST_NAME),
                     resultSet.getString(COLUMN_MIDDLE_NAME));
-                position = Position.valueOf(resultSet.getString(COLUMN_POSITION));
-                hired = resultSet.getDate(COLUMN_HIREDATE).toLocalDate();
-                salary = resultSet.getBigDecimal(COLUMN_SALARY);
+                Position position = Position.valueOf(resultSet.getString(COLUMN_POSITION));
+                LocalDate hired = resultSet.getDate(COLUMN_HIREDATE).toLocalDate();
+                BigDecimal salary = resultSet.getBigDecimal(COLUMN_SALARY);
                 BigInteger managerId = new BigInteger(String.valueOf(resultSet.getLong(COLUMN_MANAGER)));
                 int currentRow = resultSet.getRow();
                 employeeSet.add(new Employee(id, fullName, position, hired, salary,
-                    createEmployee(resultSet, managerId)));
+                    createManager(resultSet, managerId)));
 
                 resultSet.absolute(currentRow);
             }
         } catch (SQLException e) {
-            LOGGER.error(e);
+            log.error(e);
         }
         return employeeSet;
     }
 
-    public Employee createEmployee(ResultSet resultSet, BigInteger currentManagerId) throws SQLException {
+    private Employee createManager(ResultSet resultSet, BigInteger currentManagerId) throws SQLException {
         Employee newEmployee = null;
         resultSet.first();
         while (resultSet.next()) {
-            id = new BigInteger(String.valueOf(resultSet.getLong(COLUMN_ID)));
-            fullName = new FullName(
-                resultSet.getString(COLUMN_FIRST_NAME),
-                resultSet.getString(COLUMN_LAST_NAME),
-                resultSet.getString(COLUMN_MIDDLE_NAME));
-            position = Position.valueOf(resultSet.getString(COLUMN_POSITION));
-            hired = resultSet.getDate(COLUMN_HIREDATE).toLocalDate();
-            salary = resultSet.getBigDecimal(COLUMN_SALARY);
-            BigInteger newManagerId = new BigInteger(String.valueOf(resultSet.getLong(COLUMN_MANAGER)));
-
+            BigInteger id = new BigInteger(String.valueOf(resultSet.getLong(COLUMN_ID)));
             if (currentManagerId.equals(id)) {
+                FullName fullName = new FullName(
+                    resultSet.getString(COLUMN_FIRST_NAME),
+                    resultSet.getString(COLUMN_LAST_NAME),
+                    resultSet.getString(COLUMN_MIDDLE_NAME));
+                Position position = Position.valueOf(resultSet.getString(COLUMN_POSITION));
+                LocalDate hired = resultSet.getDate(COLUMN_HIREDATE).toLocalDate();
+                BigDecimal salary = resultSet.getBigDecimal(COLUMN_SALARY);
+                BigInteger newManagerId = new BigInteger(String.valueOf(resultSet.getLong(COLUMN_MANAGER)));
+
                 newEmployee = new Employee(id, fullName, position, hired, salary,
-                    createEmployee(resultSet, newManagerId));
+                    createManager(resultSet, newManagerId));
             }
         }
         return newEmployee;
